590d44a140f0feafb27eed83904677ec6de5191b
ninjamar <70116773+ninjamar@users.noreply.github.com>
2020-12-29 16:05:12 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'scratchapi3',
+  author_email = 'example@example.com',
+  version = '0.0.1',
+  classifiers = [
+      "Programming Language :: Python :: 3",
+  ],
+  description = ' scratchapi3',
+  licence = 'MIT',
+  instal_requires =['requests'],
+  packages = ['scratchapi3'],
+  author = 'ninjamar',
+  python_require = '>=3.7',
+  url = 'https://github.com/ninjamar/scratchapi3'
+)
