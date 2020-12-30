d02275fe97a659ffc73f60d9427e232d6d71df78
ninjamar <70116773+ninjamar@users.noreply.github.com>
2020-12-29 20:15:44 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'scratchapi3',
   author_email = 'example@example.com',
-  version = '0.0.1',
+  version = '0.0.2',
   classifiers = [
       "Programming Language :: Python :: 3",
   ],
