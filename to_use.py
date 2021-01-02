011f7cba7ce9eb2d8315b2cc43dd6cd480c900ad
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-01-02 11:25:12 -0700

    Update and rename test.md to to_use.py

---
+++ to_use.py
@@ -0,0 +1,12 @@
+#run 'pip install scratchapi3'
+#in code
+import scratchapi3
+scratch = Scratch('username','password',csrf_token') #look in cookies to find it. Can someone help me get the cookie data from a website using python?
+#comment
+comment = Comment('csrf_token')
+comment.profile_comment('content','parent_id','commentee_id','user') #can return
+#cloud
+cloud = Cloud()
+cloud.cloudlog('project_id','limit','offset') #returns cloud log
+#misc
+misc = misc.message('user') #returns count
