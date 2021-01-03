1988d9afb8a69dc07473fa33cf0bcbfb6d048eb2
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-01-03 11:03:45 -0700

    new version
    no user input for csrf
    added login only -beta

--- scratchapi3/__init__.py
+++ scratchapi3/__init__.py
@@ -1,32 +1,43 @@
-#get the csrf token from chrome://settings/cookies/detail?site=scratch.mit.edu&search=cookies or clicking on the cookie icon on scratch
-#to run use scratch = Login("USERNAME","PASSWORD","CSRF_TOKEN")
 import requests
+import os
+import linecache
 class Scratch:
   def get_csrf(self):
-    #response = self.session.get('https://scratch.mit.edu/') 
-    #return self.session.cookies.get_dict()
-    pass
+    URL = 'https://scratch.mit.edu/login/'
+    x = requests.get(url = URL)
+    with open('login_token.txt','a') as f:
+      f.write(x.text)
+    line = linecache.getline('login_token.txt', 228)
+    os.remove('login_token.txt')
+    return line[55:87]
   def login(self):
-    url = 'https://scratch.mit.edu/login/'
-    login_data = {self.username: self.password,"csrfmiddlewaretoken":self.csrf_token}
-    x = requests.post(url,headers={'referer': "https://scratch.mit.edu"},data=login_data)
+    URL = 'https://scratch.mit.edu/login/'
+    csrf_token = self.get_csrf()
+    login_data = {self.username: self.password,"csrfmiddlewaretoken":csrf_token}
+    x = requests.post(URL,headers={'referer': "https://scratch.mit.edu"},data=login_data)
     return x.text
 
-  def __init__(self,username,password,csrf_token):
+  def __init__(self,username,password):
     self.username = username
     self.password = password
-    self.csrf_token = csrf_token
     self.login()
-    #self.session = requests.Session()
+
 
 
 class Comment:
-  def __init__(self,csrf_token):
-    self.csrf_token = csrf_token
+  def get_csrf():
+    URL = 'https://scratch.mit.edu/login/'
+    x = requests.get(url = URL)
+    with open('login_token.txt','a') as f:
+      f.write(x.text)
+    line = linecache.getline('login_token.txt', 228)
+    os.remove('login_token.txt')
+    return line[55:87]
 
   def profile_comment(self,content,parent_id,commentee_id,user):
-    url = f'https://scratch.mit.edu/site-api/comments/user/{user}/add/'
-    data = {"content":content,"parent_id":parent_id,"commentee_id":commentee_id,"csrfmiddlewaretoken":self.csrf_token}
+    URL = f'https://scratch.mit.edu/site-api/comments/user/{user}/add/'
+    csrf_token = self.get_csrf()
+    data = {"content":content,"parent_id":parent_id,"commentee_id":commentee_id,"csrfmiddlewaretoken":csrf_token}
     x = requests.post(url=url,headers={'referer': "https://scratch.mit.edu"},data=data)
     return x.text
 
@@ -51,3 +62,23 @@ class Misc:
     x = requests.get(url = URL)
     data = x.json()
     return data[1]
+
+class Login_Only:
+  def get_csrf():
+    URL = 'https://scratch.mit.edu/login/'
+    x = requests.get(url = URL)
+    with open('login_token.txt','a') as f:
+      f.write(x.text)
+    line = linecache.getline('login_token.txt', 228)
+    os.remove('login_token.txt')
+    return line[55:87]
+  def __init__(self,my_username):
+    self.path = 'https://scratch.mit.edu'
+    self.csrf_token = self.get_csrf()
+    self.username = my_username
+
+
+  def follow(self,user):
+    pass
+  def unfollow(self,user):
+    pass
