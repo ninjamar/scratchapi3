2915e8dafe45f7bf5afa8cac86a2ab38944596fd
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-01-03 11:04:07 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'scratchapi3',
   author_email = 'example@example.com',
-  version = '0.0.9',
+  version = '1.0.0',
   classifiers = [
       "Programming Language :: Python :: 3",
   ],
   description = ' scratchapi3',
   licence = 'MIT',
-  instal_requires =['requests'],
+  instal_requires =['requests','os','linecache'],
   packages = ['scratchapi3'],
   author = 'ninjamar',
   python_require = '>=3.7',
